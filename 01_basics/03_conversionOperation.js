159b44861eb189659e80d454cc1eca67c105b849
Himanshu Saini <147541017+techhimanshu001@users.noreply.github.com>
2025-04-01 14:17:07 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score="33abc"
+
+console.log(score)
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof(score))
+console.log(valueInNumber)
+
+// "33"=> 33
+// "33abc" => NaN
+//null => 0
+//undefined => NaN
+
+
+let isloggedin = 100
+
+let booleanloggedin=Boolean(isloggedin)
+console.log(booleanloggedin)
+
+// 1=>true
+//"" => false
+//"Hitesh" => true
+// null => false
+// undefined => false
+// 1-any number =>true

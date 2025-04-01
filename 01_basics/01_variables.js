069d4213e3202f88a4efa0b0ec309f4d40ec5f75
Himanshu Saini <147541017+techhimanshu001@users.noreply.github.com>
2025-04-01 11:02:23 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 145532
+let accountEmail="xyz@gmail.com"
+var accountPass="xyz@123"
+
+accountCity="Gr."
+/* Prefer not to use var as its scope is not defined  */
+console.table([accountId,accountEmail,accountPass,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+// alert(3+3) // alert will not work as we are working in nodejs not in browser.
+
+
+let Name="Him"
+let age=18
+let islogin = true
+let relationship
+let PersonalNo = null
+
+//number => 2 to power52
+// bigint
+//string => " "
+//Number
+//boolean => true/false
+//null => to represent empty value
+//undefined => when not intialised
+//symbol => unique
+
+//object
+
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)

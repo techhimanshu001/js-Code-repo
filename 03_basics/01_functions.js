c258027755b4bbad3b29a0df07ac5d3bf0f15f3e
Himanshu Saini <147541017+techhimanshu001@users.noreply.github.com>
2025-04-22 16:53:00 +0000

    Functions and Scope

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -10,3 +10,20 @@ function add2Numbers (number1,number2){
 
 console.log(add2Numbers("3",4));
 
+function CalculateCartPrice(value1,value2,...num){
+        return num;
+}
+
+console.log(CalculateCartPrice(200,300,800,900));
+
+const student={
+        name:"Himanshu",
+        Course:"B.tech.",
+        Sec:"C"
+}
+
+function finddetails(anyObject){
+        console.log(`The username is ${anyObject.name} and the course is ${anyObject.Course}` )
+}
+
+finddetails(student);
---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,9 @@
+ if(true){
+    let a=20;
+    var b=40;
+    const c=30;
+ }
+
+ console.log(a);// SHOW ERROR BCOZ A SCOPE IS INSIDE IF BRACES
+ console.log(b);// IT WILL PRINT 40 AS VAR SCOPE IS NOT DEFINED
+ console.log(c);// SHOW ERROR BCOZ A SCOPE IS INSIDE IF BRACES
